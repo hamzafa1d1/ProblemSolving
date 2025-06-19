f2edc4edc6b7885b0c8b56198286d1695f4d3332
Hamza Faidi <hamza.faidi@expensya.com>
2025-06-19 23:10:48 +0100

    first commit

---
+++ WeirdAlgorithm.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Uncomment these for large inputs or when optimization is needed
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+// Shortcuts for common data types
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Constants
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const int MOD = 1e9+7;
+
+// Solution code
+void solve() {
+    // Your solution here
+    
+}
+
+int main() {
+    
+    // Uncomment for file I/O
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+    solve();
+    cout << "ahla bik" << endl; 
+    return 0;
+}
